9a7ccdcf6c74489dac3074ea69eca85f47da723f
Pablo Sánchez <p.sanchez94@gmail.com>
2021-11-15 23:06:47 +0100

    added modal with portals to users-project

--- react-complete-guide/users-project/src/components/AddUser.js
+++ react-complete-guide/users-project/src/components/AddUser.js
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import classes from "./AddUser.module.css";
+import Wrapper from "./Helpers/Wrapper";
 import Button from "./UI/Button";
+import ErrorModal from "./UI/ErrorModal";
 
 export default function Form(props) {
-  const [userName, setUserName] = useState('');
-  const [userAge, setUserAge] = useState('');
+  const [userName, setUserName] = useState("");
+  const [userAge, setUserAge] = useState("");
+  const [error, setError] = useState();
 
   function changeUserNameHandler(event) {
     setUserName(event.target.value);
@@ -17,44 +20,79 @@ export default function Form(props) {
   function addUserHandler(event) {
     event.preventDefault();
 
-    if (!userName || !userAge) return;
+    if (!userName || !userAge) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
+      return;
+    }
+
+    if (+userAge > 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (> 0).",
+      });
+      return;
+    }
 
     const newUser = {
       name: userName,
-      age: userAge
-    }
+      age: userAge,
+    };
 
     props.onUserSave(newUser);
 
-    setUserName('');
-    setUserAge('');
+    setUserName("");
+    setUserAge("");
+  }
+
+  function errorHandler() {
+    setError(null);
   }
 
   return (
-    <div className={classes.form}>
-      <form onSubmit={addUserHandler}>
-        <div className={`${classes["form-control"]} ${!userName && classes.invalid}`}>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            value={userName}
-            onChange={changeUserNameHandler}
-          />
-        </div>
-        <div className={`${classes["form-control"]} ${!userAge && classes.invalid}`}>
-          <label htmlFor="age">Age</label>
-          <input
-            type="number"
-            id="age"
-            min="1"
-            step="1"
-            value={userAge}
-            onChange={changeUserAgeHandler}
-          />
-        </div>
-        <Button type="submit" >Add User</Button>
-      </form>
-    </div>
+    <Wrapper>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        ></ErrorModal>
+      )}
+      <div className={classes.form}>
+        <form onSubmit={addUserHandler}>
+          <div
+            className={`${classes["form-control"]} ${
+              !userName && classes.invalid
+            }`}
+          >
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              value={userName}
+              onChange={changeUserNameHandler}
+            />
+          </div>
+          <div
+            className={`${classes["form-control"]} ${
+              !userAge && classes.invalid
+            }`}
+          >
+            <label htmlFor="age">Age</label>
+            <input
+              type="number"
+              id="age"
+              min="1"
+              step="1"
+              value={userAge}
+              onChange={changeUserAgeHandler}
+            />
+          </div>
+          <Button type="submit">Add User</Button>
+        </form>
+      </div>
+    </Wrapper>
   );
 }
---
+++ react-complete-guide/users-project/src/components/UI/ErrorModal.js
@@ -0,0 +1,43 @@
+import Button from "./Button";
+import Card from "./Card";
+import classes from "./ErrorModal.module.css";
+import ReactDOM from "react-dom";
+
+function Backdrop(props) {
+  return <div className={classes.backdrop} onClick={props.onConfirm}></div>;
+}
+
+function ModalOverlay(props) {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+}
+
+export default function ErrorModal(props) {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </>
+  );
+}
